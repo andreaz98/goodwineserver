c3c998782b239994945a3daac2ed917e1cd5dd4b
Andrea <andreaz98@vivaldi.net>
2020-06-11 15:49:42 +0200

    Initial commit per il server spring

---
+++ src/test/java/goodwine/server/GoodWineApplicationTests.java
@@ -0,0 +1,13 @@
+package goodwine.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodWineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
