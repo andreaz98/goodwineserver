788db6587fc97628f4477249bc874f5c61916e29
Andrea <andreaz98@vivaldi.net>
2020-06-15 16:43:21 +0200

    UtenteController.login() funzionante + test

--- src/main/java/goodwine/server/controller/UtenteController.java
+++ src/main/java/goodwine/server/controller/UtenteController.java
@@ -2,26 +2,22 @@ package goodwine.server.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import goodwine.server.persistenza.UtenteRepository;
 import goodwine.server.pojos.Utente;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import java.util.Optional;
 @RestController
+@RequestMapping(value = "/utente")
 public class UtenteController {
 	@Autowired 
 	UtenteRepository utenteRepo;//IoC
 	
-	/**
-	 * 
-	 * nel body della richiesta devono essere IN JSON i valori dell' utente che si vuole registrare
-	 * 
-	 * query  : /utente/registrazione
-	 * body   : json dell' utente
-	 */
-	@RequestMapping("/utente/registrazione")
+	
+	@RequestMapping(value = "/registrazione", method = RequestMethod.POST)
 	public Utente registra(@RequestBody Utente utente) {
 		String username = utente.getUsername();
 		
@@ -34,8 +30,23 @@ public class UtenteController {
 		if( u == null ) {//utente non esiste
 			System.out.println("creo utente");
 			utenteRepo.save(utente);
-			return utente;//se lo registro faccio echo al client, null altrimenti
+			return utente;//se lo registro faccio echo al client, altrimenti invio utente vuoto
 			
 		}else return new Utente();
+		
+	}
+	
+	//login molto naive
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public boolean autentica(@RequestBody Utente utente) {
+		Optional<Utente> user = utenteRepo.findById(utente.getUsername());
+		
+		if( user.isPresent() ) {
+			if( user.get().getPassword().equals(utente.getPassword()) ) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
--- src/test/java/goodwine/server/controller/test/UtenteControllerTest.java
+++ src/test/java/goodwine/server/controller/test/UtenteControllerTest.java
@@ -23,7 +23,7 @@ import goodwine.server.pojos.Utente;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.test.web.servlet.MvcResult;
 import org.junit.jupiter.api.Assertions;
-
+import goodwine.server.persistenza.UtenteRepository;
 @RunWith(SpringJUnit4ClassRunner.class)
 @AutoConfigureMockMvc //@WebMvcTest 
 @SpringBootTest
@@ -31,6 +31,9 @@ public class UtenteControllerTest {
 	
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@Autowired
+	private UtenteRepository utenteRepo;
 	
 	@Test
 	public void registrazioneUtenteSuccesso() throws Exception {
@@ -47,6 +50,45 @@ public class UtenteControllerTest {
 					
 		Assertions.assertTrue(res.getResponse().getContentAsString().contains("giggino"));
 	}
+	
+	@Test
+	public void loginSuccesso()throws Exception {
+		Utente u = new Utente();
+		u.setUsername("giggino");
+		u.setPassword("23456789");
+		
+		utenteRepo.save(u);
+		
+		MvcResult res = this.mockMvc.perform(post("/utente/login").contentType(MediaType.APPLICATION_JSON)
+							 .content(asJsonString(u)))
+				    .andDo(print())
+				    .andExpect(status().isOk())
+				    .andReturn();
+		
+		Assertions.assertTrue(res.getResponse().getContentAsString().contains("true"));
+	}
+	
+	@Test
+	public void loginFallimento()throws Exception {
+		Utente u = new Utente();
+		u.setUsername("utenteInesistentegiggino");
+		u.setPassword("23456789");
+		
+		//utenteRepo.save(u);
+		
+		MvcResult res =  this.mockMvc.perform(post("/utente/login")
+							 .contentType(MediaType.APPLICATION_JSON)
+							 .content(asJsonString(u)))
+				    .andDo(print())
+				    .andExpect(status().isOk())
+				    .andReturn();
+		
+		Assertions.assertTrue(res.getResponse().getContentAsString().contains("false"));
+	}
+	
+	
+	
+	
 	
 	
 	public static String asJsonString(final Object obj) {
